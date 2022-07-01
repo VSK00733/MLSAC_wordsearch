62755a0e6e49c3a0568ef0fa239d37b94b17085b
Vaijayanthi Sambath Kumar <97697010+VSK00733@users.noreply.github.com>
2022-07-01 23:49:43 +0530

    Update mlsac.py

--- mlsac.py
+++ mlsac.py
@@ -21,6 +21,15 @@ while p==0:
         https://bit.ly/MLSAC_Surprise
         ''')
         st.balloons()
+        st.write(f'''
+        <a target="_self" href="https://eox.at">
+        <button>
+            Please login via Google
+        </button>
+        </a>
+        ''',
+        unsafe_allow_html=True
+        )
     elif x=="":
         pass
     else:
